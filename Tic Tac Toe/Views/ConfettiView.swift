e86b48ba15cdaa138fccd2c6e7f29abcb3bc2dd6
Lai Hong Yu <joyjay606@gmail.com>
2024-11-12 15:47:25 +0800

    Done Wins segment

---
+++ Tic Tac Toe/Views/ConfettiView.swift
@@ -0,0 +1,18 @@
+//
+//  ConfettiView.swift
+//  Tic Tac Toe
+//
+//  Created by Lai Hong Yu on 11/12/24.
+//
+
+import SwiftUI
+
+struct ConfettiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConfettiView()
+}
