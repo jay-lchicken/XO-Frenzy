28d477523816470b137d35da66ceb837dda9990f
MARCUS <MARCUS_EFFENDI_XU@S2024.SSTS.EDU.SG>
2024-11-12 14:09:17 +0800

    qr3t4wrty

--- Tic Tac Toe/Views/SettingsView.swift
+++ Tic Tac Toe/Views/SettingsView.swift
@@ -13,13 +13,53 @@ struct SettingsView: View {
     var body: some View {
         Picker("Flavor", selection: $viewModel.xImageName) {
             Image(systemName: "xmark")
+                
                 .tag("xmark")
             Image(systemName: "circle")
                 .tag("circle")
-            
+            Image(systemName: "square")
+                .tag("square")
+            Image(systemName: "scissors")
+                .tag("scissors")
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .tag("person.crop.circle.fill.badge.checkmark")
+            Image(systemName: "person.crop.circle.badge.checkmark")
+                .tag("person.crop.circle.badge.checkmark")
+            Image(systemName: "star")
+                .tag("star")
+            Image(systemName: "suit.spade")
+                .tag("suit.spade")
+            Image(systemName: "suit.club")
+                .tag("suit.club")
+            Image(systemName: "star.circle")
+                .tag("star.circle")
+            Image(systemName: "flashlight.off.fill")
+                .tag("flashlight.off.fill")
+            Image(systemName: "sun.max")
+                .tag("sun.max")
+            Image(systemName: "moon.stars.fill")
+                .tag("moon.stars.fill")
+            Image(systemName: "eye")
+                .tag("eye")
+            Image(systemName: "hare")
+                .tag("hare")
+            Image(systemName: "guitars")
+                .tag("guitars")
+            Image(systemName: "tortoise")
+                .tag("tortoise")
+            Image(systemName: "ant.circle")
+                .tag("ant.circle")
+            Image(systemName: "cloud.sleet")
+                .tag("cloud.sleet")
+            Image(systemName: "person.crop.circle.badge.checkmark")
+                .tag("person.crop.circle.badge.checkmark")
+            Image(systemName: "location.north.line")
+                .tag("location.north.line")
         }
-
+       
+        .tint(Color.red)
     }
+        
 }
 
 #Preview {
