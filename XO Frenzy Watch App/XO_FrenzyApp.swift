454c2512c2f0dfa79fd9508b93c3c88b2c70e610
Lai Hong Yu <joyjay606@gmail.com>
2024-11-14 17:24:31 +0800

    Fixed

---
+++ XO Frenzy Watch App/XO_FrenzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  XO_FrenzyApp.swift
+//  XO Frenzy Watch App
+//
+//  Created by Lai Hong Yu on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct XO_Frenzy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
